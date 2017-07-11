0a2fbcc914d2b4217196c52addb3a22a35ddd778
doulighan <doulighan@gmail.com>
2017-07-11 10:35:14 -0400

    s

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Test from './test.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test />
+      </div>
+    );
+  }
+}
+
+export default App;
